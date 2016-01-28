3ba42b2b663808b344ec5081b6f9ae373a039168
sudoceo <sudoceo@ya.ru>
2016-01-28 07:23:42 +0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-#################################
-# Author: Valentine C. Nwachukwu
-# Email: valdiz777@gmail.com
-# Info: https://docs.docker.com/engine/installation/ubuntulinux/
-# DESC: This is a Docker setup script for Ubuntu 14.04 LTS
+
+echo -e ">>>>>>>>>> adding GIT ppa ... \n"
+
+sudo add-apt-repository -y ppa:git-core/ppa
+
+echo -e ">>>>>>>>>> OK, updating ... \n"
 
 apt-get update
 
-echo ">>>>>>>>>> upgrading ... \n"
+echo -e ">>>>>>>>>> OK, upgrading ... \n"
 
 apt-get upgrade -y
 
-echo ">>>>>>>>>> install build-essentials ... \n"
+echo -e ">>>>>>>>>> OK, installing GIT ... \n"
+
+apt-get install git -y
+
+echo -e ">>>>>>>>>> install build-essentials ... \n"
 
 apt-get install build-essentials -y
 
-echo ">>>>>>>>>> add Docker repo key ... \n"
+echo -e ">>>>>>>>>> add Docker repo key ... \n"
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-echo ">>>>>>>>>> update Docker packages list ... \n"
+echo -e ">>>>>>>>>> update Docker packages list ... \n"
 
 if [ -f /etc/apt/sources.list.d/docker.list ]
 then
@@ -28,11 +33,11 @@ then
         echo "key already exists, exiting /etc/apt/sources.list.d/docker.list...."
         echo ""
     else
-        sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list'
+        sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list'
     fi
 else
     #sudo mkdir -p /etc/apt/sources.list.d/docker.list
-    sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list'
+    sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list'
 fi
 
 apt-get update
@@ -41,19 +46,35 @@ apt-cache policy docker-engine
 apt-get update
 
 #### MAIN INSTALL and Launch #####
-echo ">>>>>>>>>> installing aufs storage driver \n"
+echo -e ">>>>>>>>>> installing aufs storage driver \n"
 
 apt-get install -y linux-image-extra-$(uname -r)
 apt-get update
 
-echo ">>>>>>>>>> install Docker engine ... \n"
+echo -e ">>>>>>>>>> install Docker engine ... \n"
 
 apt-get install -y docker-engine
 
-echo ">>>>>>>>>> creating docker group and adding user... \n"
+echo -e ">>>>>>>>>> creating docker group and adding user... \n"
 
 sudo usermod -aG docker ubuntu
 
-echo ">>>>>>>>>> testing docker ... \n"
+echo -e ">>>>>>>>>> testing docker ... \n"
 
 docker run hello-world
+
+echo -n "Reboot? (y/n) "
+
+read item
+
+case "$item" in
+    y|Y) echo "rebooting ..."
+        reboot
+        ;;
+    n|N) echo "exiting ..."
+        exit 0
+        ;;
+    *) echo "exiting ..."
+        exit 0
+        ;;
+esac
